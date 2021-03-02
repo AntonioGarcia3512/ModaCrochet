c1b03e831ae5b697434bba6295efc950d5f9fa50
AntonioGarcia3512 <71676381+AntonioGarcia3512@users.noreply.github.com>
2021-03-02 03:49:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const express = require('express');
 
+//Inizializations
+const app = express();
+
+//Setting
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+
+//Global Variables
+
+//Routes
+
+//Static Files
+
+//Server listening
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
+});
